9d71c3a70cf8360fdea076280961dd161793baa6
Glitch (fallacious-gainful-hole) <none>
2021-04-01 19:34:28 +0000

    🏮✍️ Checkpoint ./komutlar/otocevap-sil.js:26526049/207 ./komutlar/otorol-ayarla.js:26526049/17

--- komutlar/otocevap-sil.js
+++ komutlar/otocevap-sil.js
@@ -7,22 +7,22 @@ exports.run = async(bot, message, args) => {
 let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix 
                     
          var user = message.mentions.users.first() || message.author;
-if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.sendEmbed(new Discord.MessageEmbed().setDescription('Bu Komutu Kullanmak İçin `Sunucuyu Yönet` Yetkisi Lazım!').setColor("RED")); 
+if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.sendEmbed(new Discord.MessageEmbed().setDescription('<a:carpi:821416413138911272> Bu Komutu Kullanmak İçin `Sunucuyu Yönet` Yetkisi Lazım!').setColor("RED")); 
                 
                     
                    let komut = await db.fetch(`sunucuKomut_${message.guild.id}`) 
                 	 let gonderileceksey = await db.fetch(`sunucuMesaj_${message.guild.id}`)
-                	 if(!komut) return message.channel.send('Bu sunucuda özel komut ayarlı değil.')
+                	 if(!komut) return message.channel.send('<a:carpi:821416413138911272> Bu sunucuda özel komut ayarlı değil.')
                 	 	let komutvarmi = await db.fetch(`sunucuKomut_${message.guild.id}`)
-                	if(!args[0]) return message.channel.send(`Lütfen silmek istediğiniz özel komudu giriniz.\nMevcut özel komutlar: \`${komut}\``)
-                	if(args[0] !== komut) return message.channel.send(`Bu komut mevcut değil.\nMevcut özel komutlar: \`${komut}\``)
-                   if(args[0] == 'Bulunmuyor.') return message.channel.send(`Bu komut mevcut değil.\nMevcut özel komutlar: \`${komut}\``)
+                	if(!args[0]) return message.channel.send(`<a:carpi:821416413138911272> Lütfen silmek istediğiniz özel komudu giriniz.\nMevcut özel komutlar: \`${komut}\``)
+                	if(args[0] !== komut) return message.channel.send(`<a:carpi:821416413138911272> Bu komut mevcut değil.\nMevcut özel komutlar: \`${komut}\``)
+                   if(args[0] == 'Bulunmuyor.') return message.channel.send(`<a:carpi:821416413138911272> Bu komut mevcut değil.\nMevcut özel komutlar: \`${komut}\``)
                     
                      let welcomeEmbed = new Discord.MessageEmbed()
-                     .addField(`Bu sunucudan özel komut silindi.`, `\`${komut}\` silindi.`)
+                     .addField(`<a:mavitik:821416581216600135> Bu sunucudan özel komut silindi.`, `\`${komut}\` silindi.`)
                      .setColor('GREEN')
                 
-                     db.set(`sunucuKomut_${message.guild.id}`, 'Bulunmuyor.')
+                     db.set(`sunucuKomut_${message.guild.id}`, '<a:carpi:821416413138911272> Bulunmuyor.')
                      db.delete(`sunucuMesaj_${message.guild.id}`, gonderileceksey)
                         message.channel.send(welcomeEmbed) 
                      };
