6ba63f2630adf51710e4e6082e3abeac7dce02f6
Glitch (fallacious-gainful-hole) <none>
2021-04-01 16:23:49 +0000

    🐿💈 Checkpoint ./komutlar/moderasyon-yasaklıtag.js:26526049/289 ./komutlar/moderasyon-yavaşmod.js:26526049/267

--- komutlar/moderasyon-yasaklıtag.js
+++ komutlar/moderasyon-yasaklıtag.js
@@ -4,22 +4,22 @@ const db = require('quick.db')
 exports.run = async (client, message, args) => { 
   const fynx = require("../ayarlar/bot.json");
 let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix 
-  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`<:a_:821738957997211659>   **Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.**`);
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`<a:carpi:821416413138911272>   **Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.**`);
   
-if(!args[0]) return message.channel.send(`Yasaklı tag sistemini kullanabilmek için: ${prefix}**yasaklı-tag ekle tag** yazmalısın.`)
+if(!args[0]) return message.channel.send(`<a:carpi:821416413138911272> Yasaklı tag sistemini kullanabilmek için: ${prefix}**yasaklı-tag ekle tag** yazmalısın.`)
 let argümanlar = ['ekle', 'çıkar']
-if(!argümanlar.includes(args[0])) return message.channel.send(`Sadece ${prefix}**yasaklı-tag ekle**/**çıkar** kullanabilirsin.`)
+if(!argümanlar.includes(args[0])) return message.channel.send(`<a:carpi:821416413138911272> Sadece ${prefix}**yasaklı-tag ekle**/**çıkar** kullanabilirsin.`)
   
 if(args[0] === 'ekle') {
   
 const tag = await db.fetch(`banned-tag.${message.guild.id}`)
-if(tag) return message.channel.send(`Sadece bir tag ekleyebilirsin.`)
-if(!args[1]) return message.channel.send(`Bir tag yazmalısın.`)
+if(tag) return message.channel.send(`<a:carpi:821416413138911272> Sadece bir tag ekleyebilirsin.`)
+if(!args[1]) return message.channel.send(`<a:carpi:821416413138911272> Bir tag yazmalısın.`)
   
 await db.set(`banned-tag.${message.guild.id}`, args[1])
   
 message.channel.send(new Discord.RichEmbed()
-.setDescription(`**${args[1]}** tagı yasaklı olarak listeye eklendi.`)
+.setDescription(`<a:mavitik:821416581216600135> **${args[1]}** tagı yasaklı olarak listeye eklendi.`)
 .setColor('GREEN')
 .setAuthor(message.author.username, message.author.avatarURL)) 
 }
@@ -28,13 +28,13 @@ message.channel.send(new Discord.RichEmbed()
 if(args[0] === 'çıkar') {
   
 const tag = await db.fetch(`banned-tag.${message.guild.id}`)
-if(!tag) return message.channel.send(`Hiç tag eklememişsin.`)
-if(!args[1]) return message.channel.send(`Bir tag yazmalısın.`)
+if(!tag) return message.channel.send(`<a:carpi:821416413138911272> Hiç tag eklememişsin.`)
+if(!args[1]) return message.channel.send(`<a:carpi:821416413138911272> Bir tag yazmalısın.`)
   
 await db.delete(`banned-tag.${message.guild.id}`)
   
 message.channel.send(new Discord.RichEmbed()
-.setDescription(`**${args[1]}** tagı artık yasaklı değil..`)
+.setDescription(`<a:mavitik:821416581216600135> **${args[1]}** tagı artık yasaklı değil.`)
 .setColor('GREEN')
 .setAuthor(message.author.username, message.author.avatarURL)) 
 }
--- komutlar/moderasyon-yavaşmod.js
+++ komutlar/moderasyon-yavaşmod.js
@@ -3,22 +3,22 @@ const request = require('request')
 const fynx =require("../ayarlar/bot.json");
 const db = require("quick.db");
 exports.run = async(client, message, args) => {
-  if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send(`<:a_:821738957997211659>   **Bu komutu kullanabilmek için "\`Kanalları Yönet\`" yetkisine sahip olmalısın.**`);
+  if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send(`<a:carpi:821416413138911272>   **Bu komutu kullanabilmek için "\`Kanalları Yönet\`" yetkisine sahip olmalısın.**`);
 
   let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix 
 if (message.channel.type !== "text") return;
 const limit = args[0] ? args[0] : 0;
   if(!limit) {
               var embed = new Discord.MessageEmbed()
-                .setDescription(`<:a_:821738957997211659>  **Doğru kullanım:**   \`${prefix}yavaşmod [0/120]\``)
-                .setColor("#ffffff")
+                .setDescription(`<a:carpi:821416413138911272>  **Doğru kullanım:**   \`${prefix}yavaşmod [0/120]\``)
+                .setColor("#000000")
             message.channel.send({embed})
             return
           }
 if (limit > 120) {
-    return message.channel.send(new Discord.MessageEmbed().setDescription("**Süre limiti maksimum** **120** **saniye olabilir.**").setColor("#ffffff"));
+    return message.channel.send(new Discord.MessageEmbed().setDescription("<a:carpi:821416413138911272> **Süre limiti maksimum** **120** **saniye olabilir.**").setColor("#000000"));
 }
-   message.channel.send(new Discord.MessageEmbed().setDescription(`<:a_:821738957997211659>  **Yazma süre limiti** **${limit}** **saniye olarak ayarlanmıştır**`).setColor("#ffffff"));
+   message.channel.send(new Discord.MessageEmbed().setDescription(`<a:mavitik:821416581216600135>  **Yazma süre limiti** **${limit}** **saniye olarak ayarlanmıştır**`).setColor("#ffffff"));
 var request = require('request');
 request({
     url: `https://discordapp.com/api/v7/channels/${message.channel.id}`,
