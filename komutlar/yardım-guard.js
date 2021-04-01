ee483188d2ef193899cad2da47858737b5bc3265
Glitch (fallacious-gainful-hole) <none>
2021-04-01 20:33:19 +0000

    🌚🌸 Checkpoint ./komutlar/yardım-genel.js:26526049/266 ./komutlar/yardım-davetsistemi.js:26526049/59 ./komutlar/yardım-guard.js:26526049/527 ./komutlar/yardım-kayıtsistemi.js:26526049/344

--- komutlar/yardım-guard.js
+++ komutlar/yardım-guard.js
@@ -4,11 +4,10 @@ const fynx = require("../ayarlar/bot.json");
 exports.run = async(client, message, args) => { 
 let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix 
 let eklenti = new Discord.MessageEmbed()  
-.setAuthor(`Lord Creative Koruma Komutları`, client.user.avatarURL())
-.setColor('#ffffff')
-.setDescription(`<:a_:821738957997211659>  Lord Creative botumuzu eklemek için \`${prefix}davet\` yazabilirsiniz.`)
-.addField(`__YAKINDA__`,`<:a_:821738957997211659>  Bu Komut Çok Yakında Eklenecektir.`)
-.addField(`__Bilgilendirme__`,`<:a_:821738957997211659>  \`${prefix}davet\` | Lord Creative'yi Sunucunuza Davet Edersiniz\n<:a_:821738957997211659>  \`${prefix}botbilgi\` | Botun İstatistiklerini Gösterir \n <:a_:821738957997211659>  \`${prefix}ayarlar\` | Sunucunuzdaki Açık veya Kapalı Komutları Gösterir`)
+.setAuthor(`Vita Koruma Komutları`, client.user.avatarURL())
+.setColor('#000000')
+
+.addField(`__YAKINDA__`,` <a:carpi:821416413138911272> Bu Komut Çok Yakında Eklenecektir.`)
   .setImage(`https://geekflare.com/wp-content/uploads/2021/02/discord-bot-hosting-1200x385.jpg`)
 .setThumbnail(client.user.avatarURL)
  message.channel.send(eklenti) 
