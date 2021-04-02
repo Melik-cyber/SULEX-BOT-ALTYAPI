25509855e8bff3e1137cd329749ccbbe43c6cf69
Glitch (fallacious-gainful-hole) <none>
2021-04-02 07:58:10 +0000

    👭🚋 Checkpoint ./index.js:26526049/1782

--- index.js
+++ index.js
@@ -489,3 +489,46 @@ await user.ban() } }
 })
 
 
+
+///////////////////////////////ÇEKİLİŞ SİSTEMİ///////////////////////////////////
+
+
+
+
+bot.command({
+ name: "çekiliş",
+ code: `
+$editMessage[$getServerVar[cekilis];{title:Çekiliş Bitti 🎉}{description:Ödül: \`$sliceMessage[1]\` 🎉
+Yapan Kişi: **$userTag[$authorID]**
+Kazanan: $replaceText[$replaceText[$checkCondition[$getTextSplitLength==1];true;hiçbiri, yeterli katılımcı yoktu.];false;<@$randomText[$joinSplitText[;]]>.]}{color:RANDOM}{footer: Çekiliş Sona Erdi.:$authorAvatar}]
+$channelSendMessage[$channelID;$replaceText[$replaceText[$checkCondition[$getTextSplitLength==1];true;Yeterli katılımcı yoktu.];false;Ödülün sahibi: \`$sliceMessage[1]\` is: <@$randomText[$joinSplitText[;]]>, Tebrikler!]]
+$textSplit[$replaceText[$getReactions[$channelID;$getServerVar[cekilis];🎉;id];$clientID,;];,]
+$wait[$message[1]]
+$setServerVar[cekilis;$sendMessage[{title: Çekilişe Katılmak için  🎉 Emojisine Tıkla !.}{description: 🎉 Ödül: \`$sliceMessage[1]\` 🎉
+Yapan Kişi: **$userTag[$authorID]**
+Süre: **$message[1]**}{timestamp}{color:RED}{reactions:🎉};yes]]
+$onlyIf[$sliceMessage[1]!=;{title:Argümanlar Aranıyor}{description:Çekiliş için herhangi bir ödül koymadınız Bu Formatı Takip Edin: 
+\`\`\`
+- $getServerVar[prefix]çekiliş <süre> <ödül>.\`\`\`
+\`<> Yazmayın \`
+}{color:ORANGE}]
+$onlyIf[$isNumber[$replaceText[$replaceText[$replaceText[$replaceText[$message[1];s;];m;];h;];d;]]!=false;{title: Yanlış Kullanım}{description:Yeni girdiğiniz saat biçimi geçersiz. Bu örneği izleyin:
+\`\`\`
+1s 1 saniye 
+1m  dakika
+1h 1 saat
+1d 1 saat
+\`\`\`}{color:RED}]
+
+$onlyPerms[admin;{title:Yetkin Yok}{description:Senin \`YÖNETİCİ\` Yetkin Yok.}{color:RED}]
+`
+})
+
+
+
+bot.variables({
+    İsmi: "cekilis",
+    İsmi: "undefined"
+  }) 
+
+/////////////////////////son
