eeff7f4cfff6abad4e15ef3cc4299fa59dc0aef9
Glitch (fallacious-gainful-hole) <none>
2021-04-07 16:04:53 +0000

    💋🏔 Checkpoint ./komutlar/bot-istatistik.js:26526049/106 ./komutlar/ping.js:26526049/172

--- komutlar/bot-istatistik.js
+++ komutlar/bot-istatistik.js
@@ -6,9 +6,7 @@ const moment = require('moment');
 const ayarlar = require('../ayarlar/bot.json');
 require('moment-duration-format');
 exports.run = async(client, message, args) => {
-
 let lordcreative = new Discord.MessageEmbed()
-.setThumbnail(`https://cdn.discordapp.com/attachments/749380170351116290/750088540288712914/B0oBpM.png`)
 .addField("__**Bot Verileri**__", `<a:ayarlar:821416196675076207> **Toplam Sunucu** **|**  **${client.guilds.cache.size}** \n <a:renkliraptiye:821416816353476669>  **Toplam Kullanıcı** **|** **${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}** \n <:kanal:811645893547393096> **Toplam Kanal** **|** **${client.channels.cache.size}**`)
 .addField("__**Bot Geliştiricisi**__", `<:dev:809152110037172243>  **Bot Coderi**  <@775687856038871072> \n **DenizG#2432** \n\n <a:bit:809151528610824212> \ **Bot Geliştiricisi**  <@502882124085592065> \n **Yusuf Sesli#5119** \n\n <:kategori:809152540763095100>  **Bot Sahibi**  <@775687856038871072> \n **DenizG#2432** \n :heart: **Bot Destekçileri:** <@796751624823373865>, <@719536627516571658> `)
 .addField("__**Sürümler**__", `<a:aagifbase_:785962135565762601> **Discord.js Sürümü** **|**  **v${Discord.version}** \n<a:Verified:814153668282155058>  **Node.js Sürümü** **|**  **${process.version}**`)
