180fd5229f5c132e69301904958850f0249653c1
Glitch (fallacious-gainful-hole) <none>
2021-04-09 16:17:07 +0000

    🍳🐾 Checkpoint ./komutlar/bot-istatistik.js:26526049/275

--- komutlar/bot-istatistik.js
+++ komutlar/bot-istatistik.js
@@ -7,12 +7,13 @@ const ayarlar = require('../ayarlar/bot.json');
 require('moment-duration-format');
 exports.run = async(client, message, args) => {
 let lordcreative = new Discord.MessageEmbed()
+.setThumbnail('https://cdn.discordapp.com/attachments/828263419194507275/830112334058881044/images.jpg')
 .addField("__**Bot Verileri**__", `<a:ayarlar:821416196675076207> **Toplam Sunucu** **|**  **${client.guilds.cache.size}** \n <a:renkliraptiye:821416816353476669>  **Toplam Kullanıcı** **|** **${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}** \n <:kanal:811645893547393096> **Toplam Kanal** **|** **${client.channels.cache.size}**`)
 .addField("__**Bot Geliştiricisi**__", `<:dev:809152110037172243>  **Bot Coderi**  <@775687856038871072> \n **DenizG#2432** \n\n <a:bit:809151528610824212> \ **Bot Geliştiricisi**  <@502882124085592065> \n **Yusuf Sesli#5119** \n\n <:kategori:809152540763095100>  **Bot Sahibi**  <@775687856038871072> \n **DenizG#2432** \n :heart: **Bot Destekçileri:** <@796751624823373865>, <@719536627516571658> `)
 .addField("__**Sürümler**__", `<a:aagifbase_:785962135565762601> **Discord.js Sürümü** **|**  **v${Discord.version}** \n<a:Verified:814153668282155058>  **Node.js Sürümü** **|**  **${process.version}**`)
 .addField("__**Gecikmeler**__", `<a:renklipartner:821416795436482661>  **${client.ws.ping}** ms`,true)
 .addField("**__Müzik Oynatılan__** ", "<a:an:809151469655425127>  " +client.voice.connections.size + " Sunucu", true)
-    .setImage(`https://geekflare.com/wp-content/uploads/2021/02/discord-bot-hosting-1200x385.jpg`)
+    .setImage(`https://cdn.discordapp.com/attachments/829274706393169942/829326128232923146/standard.gif`)
 .setColor("#000000")
 message.channel.send(lordcreative)
 }
