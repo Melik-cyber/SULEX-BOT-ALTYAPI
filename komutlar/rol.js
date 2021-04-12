84faac17666547a39d2b1a77acb5718d38ab1b4b
Glitch (fallacious-gainful-hole) <none>
2021-04-12 06:09:57 +0000

    🏃🙏 Checkpoint ./komutlar/rol.js:26526049/357

--- komutlar/rol.js
+++ komutlar/rol.js
@@ -9,14 +9,13 @@ let eklenti = new Discord.MessageEmbed()
 .addField(`__BDFD Kodları İçin : __`,`<:BDFD:830828954591559711> \`Tıklayabilirsiniz!\``,true)
 .addField(`__HTML Kodları İçin : __`,`<:html:830829903146123295>  \`Tıklayabilirsiniz!\` `,true)
 .addField(`__DBD.JS Kodları İçin : __`,`<:dbdjs:809151911470039160>  \`Tıklayabilirsiniz!\`  `,true)
-.addField(`__Müzik Komutları__`,`<:kategori:809152540763095100>  \`${prefix}müzik\` `,true)
-.addField(`__Eklenti Komutları__`,`<a:ayarlar:821416196675076207>  \`${prefix}eklenti\`  `,true)
-.addField(`__Prefix Değiştir__`,`<:simseq:821416901086806027>  \`${prefix}prefix\` `,true)
+.addField(`__Python Kodları İçin : __`,`<:python:830829016797020161>  \`Tıklayabilirsiniz!\` `,true)
+.addField(`__JS Kodları İçin : __`,`<:nodejs:830828876900597800>  \`Tıklayabilirsiniz!\`  `,true)
   .setImage(`https://cdn.discordapp.com/attachments/829274706393169942/829326128232923146/standard.gif`)
 .setThumbnail(client.user.avatarURL)
  message.channel.send(eklenti) 
   };
 exports.config = {
-name: "yardım",
+name: "code",
   aliases: []
 }
